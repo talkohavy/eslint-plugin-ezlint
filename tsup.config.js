3e220310ffb61f7f0a6d89d5646b8189b6868a1c
Tal Kohavy <talkohavy@gmail.com>
2024-01-14 19:10:12 +0200

    went back to tsup - output is lighter

---
+++ tsup.config.js
@@ -0,0 +1,59 @@
+import fs from 'fs';
+import { defineConfig } from 'tsup';
+
+// The options here is derived from CLI flags.
+const tsupConfig = defineConfig((_options) => ({
+  entry: ['src/index.js'],
+  outDir: 'dist', // <--- defaults to dist
+  bundle: true,
+  minify: true, // <--- You can minify the output, resulting into lower bundle sizes.
+  format: ['cjs'], // <-- If package.json type is set to module, the filenames are: [.js,.cjs], else: [.mjs, .js].
+  clean: true, // <--- Should clean output directory before each build?
+  treeshake: true, // <--- esbuild has tree shaking enabled by default, but sometimes it's not working very well, so tsup offers an additional option to let you use Rollup for tree shaking instead. This flag will enable Rollup for tree shaking.
+  // publicDir: './src',
+  // sourcemap: true, // <-- If you don't minify you don't need sourcemaps! This will emit a ./dist/index.js.map.
+  // splitting: true, // <--- defaults to true. Code splitting currently only works with the esm output format.
+  // dts: true, // <--- Generate declaration file, meaning a index.d.ts.
+  // target: , // <--- The value for target defaults to compilerOptions.target in your tsconfig.json, or node14 if unspecified. For more information check out esbuild's target option.
+  // env: process.NODE_ENV,
+  async onSuccess() {
+    copyTheReadmeFile();
+
+    copyTheNpmrcFile();
+
+    copyThePackageJsonFile();
+
+    // Step 5: run the cleanup function
+    return () => {
+      // cleanupFunction()
+      console.log('DONE !!!');
+    };
+  },
+}));
+
+function copyTheReadmeFile() {
+  const readStreamReadmeMd = fs.createReadStream('./README.md');
+  const writeStreamReadmeMd = fs.createWriteStream('./dist/README.md');
+  readStreamReadmeMd.pipe(writeStreamReadmeMd);
+}
+
+function copyTheNpmrcFile() {
+  const readStreamNpmrc = fs.createReadStream('./.npmrc');
+  const writeStreamNpmrc = fs.createWriteStream('./dist/.npmrc');
+  readStreamNpmrc.pipe(writeStreamNpmrc);
+}
+
+function copyThePackageJsonFile() {
+  const packageJson = JSON.parse(fs.readFileSync('./package.json').toString());
+
+  // - Remove all scripts
+  delete packageJson.scripts;
+
+  // - Change from private to public
+  delete packageJson.private;
+  packageJson.publishConfig.access = 'public';
+
+  fs.writeFileSync('./dist/package.json', JSON.stringify(packageJson));
+}
+
+export default tsupConfig;
