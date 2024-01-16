449b2b41728177deed609e4194c7cbecb1a9b37f
Tal Kohavy <talkohavy@gmail.com>
2024-01-16 23:02:24 +0200

    more concise config organization

--- src/index.js
+++ src/index.js
@@ -1,59 +1,9 @@
-import { baseRules, exportRules, importRules, jsdocRules, typescriptRules } from './rules/common/index';
+import { recommendedConfig } from './rules/common/index.js';
+import { reactConfig } from './rules/react/index.js';
 
 export default {
   configs: {
-    recommended: {
-      extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
-      plugins: ['import', 'sort-exports', 'jsdoc', '@typescript-eslint'],
-      settings: {
-        // This is from the documentation of `eslint-import-resolver-typescript`:
-        // - Step 1: define import/parsers
-        'import/parsers': {
-          '@typescript-eslint/parser': ['*.ts', '*.d.ts', '*.tsx'],
-          html: ['*.html'],
-          // espree: ['*.js', '*.jsx'], // "espree" is the default parser eslint uses.
-        },
-
-        // - Step 2: define import/resolver
-        'import/resolver': {
-          typescript: {
-            alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
-
-            // project: './jsconfig.json', // use <root>/path/to/folder/tsconfig.json
-
-            // Or, use multiple tsconfigs (Useful for monorepos):
-            // -- way No. 1: use a glob pattern
-            // project: 'packages/*/tsconfig.json',
-
-            // -- way No. 2: use an array
-            // project: ['packages/module-a/tsconfig.json', 'packages/module-b/tsconfig.json'],
-          },
-        },
-      },
-      env: { node: true, browser: true, jest: true, es6: true },
-      parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-      parser: '@typescript-eslint/parser',
-      overrides: [
-        {
-          files: ['*.ts'],
-          rules: { 'jsdoc/require-param': 'off' },
-        },
-        {
-          files: ['*.d.ts'],
-          rules: { '@typescript-eslint/no-unused-vars': 'off' },
-        },
-      ],
-      rules: {
-        ...baseRules,
-        ...importRules,
-        ...exportRules,
-        ...jsdocRules,
-        ...typescriptRules,
-        'no-constant-condition': 'off',
-      },
-    },
-    react: {
-      extends: ['plugin:react/all'],
-    },
+    recommended: recommendedConfig,
+    react: reactConfig,
   },
 };
--- src/rules/common/index.js
+++ src/rules/common/index.js
@@ -1,5 +1,56 @@
-export { baseRules } from './base.rules';
-export { exportRules } from './export.rules';
-export { importRules } from './import.rules';
-export { jsdocRules } from './jsdoc.rules';
-export { typescriptRules } from './typescript.rules';
+import { baseRules } from './base.rules';
+import { exportRules } from './export.rules';
+import { importRules } from './import.rules';
+import { jsdocRules } from './jsdoc.rules';
+import { typescriptRules } from './typescript.rules';
+
+export const recommendedConfig = {
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended'],
+  plugins: ['import', 'sort-exports', 'jsdoc', '@typescript-eslint'],
+  settings: {
+    // This is from the documentation of `eslint-import-resolver-typescript`:
+    // - Step 1: define import/parsers
+    'import/parsers': {
+      '@typescript-eslint/parser': ['*.ts', '*.d.ts', '*.tsx'],
+      html: ['*.html'],
+      // espree: ['*.js', '*.jsx'], // "espree" is the default parser eslint uses.
+    },
+
+    // - Step 2: define import/resolver
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true, // always try to resolve types under `<root>@types` directory even it doesn't contain any source code, like `@types/unist`
+
+        // project: './jsconfig.json', // use <root>/path/to/folder/tsconfig.json
+
+        // Or, use multiple tsconfigs (Useful for monorepos):
+        // -- way No. 1: use a glob pattern
+        // project: 'packages/*/tsconfig.json',
+
+        // -- way No. 2: use an array
+        // project: ['packages/module-a/tsconfig.json', 'packages/module-b/tsconfig.json'],
+      },
+    },
+  },
+  env: { node: true, browser: true, jest: true, es6: true },
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  parser: '@typescript-eslint/parser',
+  overrides: [
+    {
+      files: ['*.ts'],
+      rules: { 'jsdoc/require-param': 'off' },
+    },
+    {
+      files: ['*.d.ts'],
+      rules: { '@typescript-eslint/no-unused-vars': 'off' },
+    },
+  ],
+  rules: {
+    ...baseRules,
+    ...importRules,
+    ...exportRules,
+    ...jsdocRules,
+    ...typescriptRules,
+    'no-constant-condition': 'off',
+  },
+};
